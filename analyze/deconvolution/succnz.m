9d0331281b20dff3c78b7eaaf6b4bf9a543a00e2
Tristan Stenner <ttstenner@gmail.com>
2015-10-29 10:31:43 +0100

    Speed up and comment succnz

--- analyze/deconvolution/succnz.m
+++ analyze/deconvolution/succnz.m
@@ -1,27 +1,32 @@
 function snz = succnz(data, crit, fac, sr)
+% succnz calculates an index of how many successive values are
+% above the parameter crit as described in section 4.3 of
+% Benedek, M. & Kaernbach, C. (2010). A continuous measure of phasic
+% electrodermal activity. J. Neurosci. Methods, 190, 80–91.
 
-nzL = [];
-cntr = 0;
 n = length(data);
 
-for i = 1:n
-    if abs(data(i)) > crit
-        cntr = cntr + 1;
-    else
-        if cntr > 0
-            nzL = [nzL, cntr];
-            cntr = 0;
-        end
-    end
+abovecrit = abs(data) > crit;
+nzidx = find(diff(abovecrit)) + 1;
 
-end
-
-if cntr > 0
-    nzL = [nzL, cntr];
-end
-
-if ~isempty(nzL)
-    snz = sum((nzL/sr).^fac)/(n/sr);
-else
+if isempty(nzidx)
     snz = 0;
+    return
 end
+
+% if the sequence begins with a value above crit prepend 1
+if abovecrit(1) == 1
+    nzidx = [1 nzidx];
+end
+% if the sequence ends with a value above crit append the length
+if abovecrit(end) == 1
+    nzidx = [nzidx n+1];
+end
+
+% now nzidx contains every position where data rises above crit (odd
+% indices) or dips below crit (even indices).
+% The lengths of spans above crit is the difference between the start index
+% and the end index
+nzL = nzidx(2:2:end) - nzidx(1:2:end);
+
+snz = sum((nzL/sr).^fac)/(n/sr);
