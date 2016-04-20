deb2f090ef125df9bc742f7cf1d3165f702dfdf0
Tristan Stenner <ttstenner@gmail.com>
2016-04-20 14:44:57 +0200

    Fix Biopac .mat-import for files without event channel

--- main/import/getBiopacMatData.m
+++ main/import/getBiopacMatData.m
@@ -7,19 +7,19 @@ conductance = conductance - min(conductance) + 1;
 sr = 1000/D.isi;
 time = (1:length(conductance))/sr;
 
-ev_s = D.data(:,end);
-
-clear D;
-
-
-[onsetL, durationL, eventTypeL] = readEventSignal(ev_s, sr);
-
-
 %setup event struct
 event = [];
-for iEvent = 1:length(eventTypeL)
-    event(iEvent).time = onsetL(iEvent);
-    event(iEvent).nid = eventTypeL(iEvent);
-    event(iEvent).name = ['T',num2str(eventTypeL(iEvent))];
-    event(iEvent).userdata.duration = durationL(iEvent);
+
+if size(D.data,2)>1
+    ev_s = D.data(:,end);
+    clear D;
+
+    [onsetL, durationL, eventTypeL] = readEventSignal(ev_s, sr);
+
+    for iEvent = 1:length(eventTypeL)
+        event(iEvent).time = onsetL(iEvent);
+        event(iEvent).nid = eventTypeL(iEvent);
+        event(iEvent).name = ['T',num2str(eventTypeL(iEvent))];
+        event(iEvent).userdata.duration = durationL(iEvent);
+    end
 end
